8c16001cfbbfb23da61078df50528c74b27a7da9
parbez <imranbarbhuiya.fsd@gmail.com>
2024-02-06 22:28:32 +0530

    chore: hello github

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,13 +4,12 @@ export default defineConfig({
 	clean: true,
 	dts: true,
 	entry: ['src/index.ts'],
-	format: ['esm', 'cjs', 'iife'],
+	format: ['esm', 'cjs'],
 	minify: false,
 	skipNodeModulesBundle: true,
 	sourcemap: true,
 	target: 'esnext',
 	keepNames: true,
-	globalName: 'template',
 	tsconfig: 'src/tsconfig.json',
-	treeshake: true
+	treeshake: true,
 });
